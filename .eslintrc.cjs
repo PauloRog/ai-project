b03277d89ad4baccb8e66a428c459a7fca77157a
PauloRog <paulo-rpf@hotmail.com>
2023-09-12 10:36:36 -0400

    feat: Build the transcription system interface.

---
+++ .eslintrc.cjs
@@ -0,0 +1,38 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', '@typescript-eslint/eslint-plugin', 'eslint-plugin-import-helpers'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    '@typescript-eslint/no-namespace': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'prettier/prettier': 'error',
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        'newlinesBetween': 'always',
+        'groups': ['module', '/^@shared/', ['parent', 'sibling', 'index']],
+        'alphabetize': { 'order': 'asc', 'ignoreCase': true }
+      }
+    ]
+  },
+  settings: {
+    'import/resolver': {
+      'typescript': {}
+    }
+  }
+}
